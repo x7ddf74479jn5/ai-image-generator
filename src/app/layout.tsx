d5d7f33bd480e91edf7052bf6c39de7faa569919
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-28 00:36:25 +0900

    ✨ feat: PromptInput UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Header />
-
         <PromptInput />
-
         {children}
       </body>
     </html>
--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useState } from "react";
 export const PromptInput = () => {
-  return <div>PromptInput</div>;
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="m-10">
+      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+        <textarea
+          placeholder="Enter a prompt..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="flex-1 p-4 outline-none rounded-md"
+        />
+        <button
+          type="submit"
+          disabled={!input}
+          className={`p-4 font-bold ${
+            input ? "bg-violet-500 text-white transition-colors duration-200" : "text-gray-300 cursor-not-allowed"
+          }`}
+        >
+          Generate
+        </button>
+        <button
+          type="button"
+          className="p-4 Ubg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400"
+        >
+          Use Suggestion
+        </button>
+        <button
+          type="button"
+          className="p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold"
+        >
+          New Suggestion
+        </button>
+      </form>
+    </div>
+  );
 };
